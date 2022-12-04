dc256c6e211598f493702efe15a533b8e60e8177
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2022-12-04 14:37:16 +0100

    started on/off switch for filters

--- App/js/main.js
+++ App/js/main.js
@@ -2,22 +2,19 @@ const App = {
   init() {
     this.cacheElements();
     this.addEventListener();
+    this.onoff();
   },
 
   cacheElements() {
-    console.log('cacheElements');
+    // console.log('cacheElements');
 
     this.splashScreen = document.querySelector(".splash-screen");
     this.splashBtn = document.querySelector(".splash-btn");
     this.mainScreen = document.querySelector(".main-screen");
-
-    // console.log(this.splashScreen.value);
-    // console.log(this.splashBtn);
-    // console.log(this.mainScreen);
   },
 
   addEventListener() {
-    console.log('addEventListener');
+    // console.log('addEventListener');
 
     // add event listener to splash button
     this.splashBtn.addEventListener('click', () => {
@@ -25,6 +22,17 @@ const App = {
       this.mainScreen.classList.remove("hidden");
     });
   },
+
+  onoff(){
+    console.log('on off test');
+    
+    currentvalue = document.getElementById('onoff').value;
+    if(currentvalue == "Off"){
+      document.getElementById("onoff").value="On";
+    }else{
+      document.getElementById("onoff").value="Off";
+    }
+  }
 };
 
 App.init();
